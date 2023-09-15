26e4ef6251e26c3c7ad9a270d00af629653935b4
Enrico Tessari <tessari91@gmail.com>
2023-09-15 13:02:17 +0200

    init repo

---
+++ charts/envoyproxy-ratelimit/README.md
@@ -0,0 +1,3 @@
+# Envoyproxy Ratelimit Global Rate Limit Helm Chart
+
+This repo contains a basic Helm chart that deploys [envoyproxy/ratelimit](https://github.com/envoyproxy/ratelimit) to a Kubernetes cluster.
---
+++ charts/envoyproxy-ratelimit/templates/_helpers.go
@@ -0,0 +1,18 @@
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "ratelimit.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Format pdb.
+*/}}
+{{- define "pdb.formatValue" }}
+{{- $valueToFormat := . | toString }}
+{{- if contains "%" $valueToFormat }}
+{{- print $valueToFormat | quote }}
+{{- else }}
+{{- print $valueToFormat }}
+{{- end }}
+{{- end }}
